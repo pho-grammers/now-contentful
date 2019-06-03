7e986f996db08039cd9d846bf28b70596e9daed5
Kevin K. Pho <kevinpho@mit.edu>
2019-06-02 23:33:54 -0400

    [Test] Whatever

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 index.js
 
 [Author] Kevin K. Pho (OmnInfinity) <kevinpho@mit.edu>
+[Author] Teresa T. Pho (teresapho5) <teresathanhpho@gmail.com>
 [Description] Main entry point for the UIHook
  */
 
@@ -9,15 +10,6 @@ index.js
 const {withUiHook, htm} = require("@zeit/integration-utils");
 
 /*** Types ***/
-// let count = 0;
-
-// // Store the current results here
-// // [Note] Constant structure (with variables inside)
-// const store = {
-//   secretId: "",
-//   secretKey: ""
-// };
-
 module.exports = withUiHook(
   // Take an async anonymous lambda that takes a payload
   async ({payload, zeitClient}) => {
@@ -25,39 +17,39 @@ module.exports = withUiHook(
     const {clientState, action} = payload;
 
     // Download the metadata
-    const store = await zeitClient.getMetadata();
+    const api = await zeitClient.getMetadata();
 
     if (action === "submit") {
       // console.log("%s %s %s %s", store.secretId, store.secretKey, clientState.secretId, clientState.secretKey)
+
       // Update the stored value with the client's inputs
-      store.secretId = clientState.secretId;
-      store.secretKey = clientState.secretKey;
+      api.Base = "https://cdn.contentful.com";
+      api.SpaceId = clientState.SpaceId;
+      api.Key = clientState.Key;
 
       // Push these changes
-      await zeitClient.setMetadata(store);
+      await zeitClient.setMetadata(api);
     }
 
-    else if (action === "reset") {
-      store.secretId = "";
-      store.secretKey = "";
-
-      // Push these changes
-      await zeitClient.setMetadata(store);
-    }
-
-    // [Note] JSX does not allow spaces between properties
-    return "asd";
-    // htm`
-    //   <Page>
-    //     <Container>
-    //       <Input label="Secret ID" name="secretId" value=${store.secretId || ""}/>
-    //       <Input label="Secret Key" name="secretKey" type="password" value=${store.secretKey || ""}/>
-    //     </Container>
-    //     <Container>
-    //       <Button action="submit">Submit</Button>
-    //       <Button action="reset">Reset</Button>
-    //     </Container>
-    //   </Page>
-    // `;
+    return htm`
+      <Page>
+        <Container>
+          <Input label="Space ID" name="spaceId" value=${api.spaceId || ""}/>
+          <Input label="Secret Key" name="secretKey" type="password" value=${store.secretKey || ""}/>
+        </Container>
+        <Container>
+          <Button action="submit">Submit</Button>
+          <Button action="reset">Reset</Button>
+        </Container>
+          <Container>
+            <Input label="Space ID" name="spaceId" value=${api.spaceId || ""}/>
+            <Input label="Secret Key" name="secretKey" type="password" value=${store.secretKey || ""}/>
+          </Container>
+          <Container>
+            <Button action="submit">Submit</Button>
+            <Button action="reset">Reset</Button>
+          </Container>
+      </Page>
+    `;
   }
 );
